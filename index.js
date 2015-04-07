4b817060a5d3d053fd5247012787e1223f127a93
toby <git@toby.org.za>
2015-04-07 11:40:10 +0200

    Added a token option in order to support temporary credentials (ie. key, secret and token) obtained from the amazon sts service

--- index.js
+++ index.js
@@ -37,7 +37,8 @@ module.exports = function SkipperS3 (globalOpts) {
         secret: globalOpts.secret,
         bucket: globalOpts.bucket,
         region: globalOpts.region||undefined,
-        endpoint: globalOpts.endpoint||undefined
+        endpoint: globalOpts.endpoint||undefined,
+        token: globalOpts.token||undefined
       });
 
       // Build a noop transform stream that will pump the S3 output through
@@ -104,7 +105,8 @@ module.exports = function SkipperS3 (globalOpts) {
         secret: globalOpts.secret,
         bucket: globalOpts.bucket,
         region: globalOpts.region,
-        endpoint: globalOpts.endpoint
+        endpoint: globalOpts.endpoint,
+        token: globalOpts.token||undefined
       });
 
       // TODO: take a look at maxKeys
@@ -230,7 +232,8 @@ module.exports = function SkipperS3 (globalOpts) {
           secret: options.secret,
           bucket: options.bucket,
           region: globalOpts.region||undefined,
-          endpoint: globalOpts.endpoint||undefined
+          endpoint: globalOpts.endpoint||undefined,
+          token: globalOpts.token||undefined
         })
       }, function (err, body) {
         if (err) {
