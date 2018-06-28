c9ded1adb116f9808254ad7d80bb2adf0f08cf35
Mike McNeil <git+mikermcneil@sailsjs.com>
2018-06-28 18:03:38 -0500

    experimental ls

--- experimental-ls.js
+++ experimental-ls.js
@@ -1,1 +1,47 @@
 var AWS = require('aws-sdk');
+
+// Based on:
+// https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#listObjectsV2-property
+
+var optionalPrefix = undefined;
+var optionalMaxKeys = undefined;
+var awsAccessKey = process.argv[2];
+var awsSecret = process.argv[3];
+var bucketName = 'experiment-jun28-2018';
+
+console.log('Using AWS access key:', awsAccessKey);
+console.log('Using AWS secret:', awsSecret);
+console.log('Using bucket:', bucketName);
+console.log('Listing files in S3 with optional prefix:', optionalPrefix);
+console.log('Max keys is set to:', optionalMaxKeys);
+
+var s3 = new AWS.S3({
+  apiVersion: '2006-03-01',
+  region: 'us-west-2',
+  accessKeyId: awsAccessKey,
+  secretAccessKey: awsSecret
+});
+
+
+var s3LsArgins = {
+  Bucket: bucketName,
+  MaxKeys: optionalMaxKeys,
+  Prefix: optionalPrefix
+};
+for (let k in s3LsArgins) {
+  if (s3LsArgins[k] === undefined) {
+    delete s3LsArgins[k];
+  }
+}
+
+
+s3.listObjectsV2(s3LsArgins, (err, result)=>{
+  if (err){
+    console.error('s3 ls error:',err);
+    return;
+  }
+
+  console.log('s3 ls success!', result['Contents']);
+
+});//_∏_
+
