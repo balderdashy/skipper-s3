982fe235486a1b95d57c8a7c9cb6051b88129701
Mike McNeil <git+mikermcneil@sailsjs.com>
2018-07-11 18:57:27 -0500

    Make quota code backwards-compatible.

--- index.js
+++ index.js
@@ -104,26 +104,54 @@ module.exports = function SkipperS3 (globalOpts) {
       s3ClientOpts = s3ClientOpts || {};
       s3ClientOpts = _.extend({}, globalOpts, s3ClientOpts);
 
+      var wasMaxBytesPerUpstreamQuotaExceeded;
+      var wasMaxBytesPerFileQuotaExceeded;
+      var maxBytesPerUpstream = s3ClientOpts.maxBytes || undefined;
+      var maxBytesPerFile = s3ClientOpts.maxBytesPerFile || undefined;
+
       var receiver = Writable({ objectMode: true });
       receiver.once('error', (unusedErr)=>{
         // console.log('ERROR ON receiver ::', unusedErr);
       });//œ
 
+      var bytesWrittenByFd = {};
+
       receiver._write = (incomingFileStream, encoding, proceed)=>{
+
+        bytesWrittenByFd[incomingFileStream.fd] = 0;//« bytes written for this file so far
         incomingFileStream.once('error', (unusedErr)=>{
           // console.log('ERROR ON incoming readable file stream in Skipper S3 adapter (%s) ::', incomingFileStream.filename, unusedErr);
         });//œ
         _uploadFile(incomingFileStream, (progressInfo)=>{
+          bytesWrittenByFd[incomingFileStream.fd] = progressInfo.written;
           incomingFileStream.byteCount = progressInfo.written;//« used by Skipper core
-          // TODO: for compatibility: set up upload quota based on total bytes for the entire upstream
-          // ^FUTURE: provide some other way of enforcing quotas on a per-file basis (would need to be changed in skipper core though, too.  And it's a bit of a heavy lift, since them's some complicated codes)
-          receiver.emit('progress', progressInfo);
-        }, s3ClientOpts, (err, report)=>{
+          let totalBytesWrittenForThisUpstream = 0;
+          for (let fd in bytesWrittenByFd) {
+            totalBytesWrittenForThisUpstream += bytesWrittenByFd[fd];
+          }//∞
+          if (maxBytesPerUpstream && totalBytesWrittenForThisUpstream > maxBytesPerUpstream) {
+            wasMaxBytesPerUpstreamQuotaExceeded = true;
+            return false;
+          } else if (maxBytesPerFile && bytesWrittenByFd[incomingFileStream.fd] > maxBytesPerFile) {
+            wasMaxBytesPerFileQuotaExceeded = true;
+            return false;
+          } else {
+            receiver.emit('progress', progressInfo);
+            return true;
+          }
+        }, s3ClientOpts, (err)=>{
           if (err) {
             // console.log(('Receiver: Error writing `' + __newFile.filename + '`:: ' + require('util').inspect(err) + ' :: Cancelling upload and cleaning up already-written bytes...').red);
+            if (flaverr.taste({name: 'RequestAbortedError'}, err)) {
+              if (maxBytesPerUpstream && wasMaxBytesPerUpstreamQuotaExceeded) {
+                err = flaverr({code: 'E_EXCEEDS_UPLOAD_LIMIT'}, new Error(`Upload too big!  Exceeded quota ("maxBytes": ${maxBytesPerUpstream})`));
+              } else if (maxBytesPerFile && wasMaxBytesPerFileQuotaExceeded) {
+                err = flaverr({code: 'E_EXCEEDS_FILE_SIZE_LIMIT'}, new Error(`One of the attempted file uploads was too big!  Exceeded quota ("maxBytesPerFile": ${maxBytesPerFile})`));
+              }//ﬁ
+            }//ﬁ
             receiver.emit('error', err);
           } else {
-            incomingFileStream.byteCount = report.totalBytesWritten;//« used by Skipper core
+            incomingFileStream.byteCount = bytesWrittenByFd[incomingFileStream.fd];//« used by Skipper core
             receiver.emit('writefile', incomingFileStream);
             return proceed();
           }
@@ -163,11 +191,7 @@ function _buildS3Client(s3ClientOpts) {
   return new AWS.S3(s3ConstructorArgins);
 }//ƒ
 
-function _uploadFile(incomingFileStream, onProgress, s3ClientOpts, done) {
-
-  var wasMaxBytesQuotaExceeded;
-  var totalBytesWritten = 0;
-
+function _uploadFile(incomingFileStream, onProgress, handleProgress, s3ClientOpts, done) {
   // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#upload-property
   var s3ManagedUpload = _buildS3Client(s3ClientOpts)
   .upload(_stripKeysWithUndefinedValues({
@@ -176,34 +200,28 @@ function _uploadFile(incomingFileStream, onProgress, s3ClientOpts, done) {
     Body: incomingFileStream,
     ContentType: mime.lookup(incomingFileStream.fd)//« advisory; makes things nicer in the S3 dashboard
   }), (err, rawS3ResponseData)=>{
-    if (err && s3ClientOpts.maxBytes && wasMaxBytesQuotaExceeded && flaverr.taste({name: 'RequestAbortedError'}, err)) {
-      err = flaverr({code: 'E_EXCEEDS_UPLOAD_LIMIT'}, new Error(`Upload too big!  Exceeded quota ("maxBytes": ${s3ClientOpts.maxBytes})`));
-      return done(err);
-    } else if (err) {
+    if (err) {
       return done(err);
     } else {
       return done(undefined, {
-        rawS3ResponseData,
-        totalBytesWritten
+        rawS3ResponseData
       });
     }
   });//_∏_
 
   s3ManagedUpload.on('httpUploadProgress', (event)=>{
-    totalBytesWritten = event.loaded;
-    if (event.loaded > s3ClientOpts.maxBytes) {
-      wasMaxBytesQuotaExceeded = true;
+    let written = _.isNumber(event.loaded) ? event.loaded : 0;
+    let total = _.isNumber(event.total) ? event.total : undefined;
+    let handledSuccessfully = handleProgress(_stripKeysWithUndefinedValues({
+      name: incomingFileStream.filename || incomingFileStream.fd,
+      fd: incomingFileStream.fd,
+      written,
+      total,
+      percent: total ? (written / total) : undefined
+    }));
+    if (!handledSuccessfully) {
       s3ManagedUpload.abort();
-    } else if (onProgress) {
-      let written = _.isNumber(event.loaded) ? event.loaded : 0;
-      let total = _.isNumber(event.total) ? event.total : undefined;
-      onProgress(_stripKeysWithUndefinedValues({
-        name: incomingFileStream.filename || incomingFileStream.fd,
-        written,
-        total,
-        percent: total ? (written / total) : undefined
-      }));
-    }//ﬁ
+    }
   });//œ
 
 }//ƒ
